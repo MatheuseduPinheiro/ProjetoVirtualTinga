08706e4e152c50bbf9edd6f331517d3ebe836a9e
Matheus Pinheiro <100390250+MatheuseduPinheiro@users.noreply.github.com>
2025-01-20 17:56:19 -0300

    Modificacao Semantica (Inicio para Home)

--- Assets/Scripts/SceneHome.cs
+++ Assets/Scripts/SceneHome.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneInicio : MonoBehaviour
+public class SceneHome : MonoBehaviour
 {
 
     // Vai pra regra
